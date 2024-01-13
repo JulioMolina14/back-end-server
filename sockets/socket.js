3c98a8ed5c5403ab2eb7e391ca9fe503f110864a
JulioMolina14 <respaldo863@gmail.com>
2024-01-13 11:15:35 -0500

    backen-server-full

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,21 @@
 
 const {io} = require('../index');
+const Band = require('../models/band');
+
+const Bands = require('../models/bands');
+
+const bandas= new Bands();
+bandas.addBand(new Band('Queen'));
+bandas.addBand(new Band('Metalica'));
+bandas.addBand(new Band('Link Park'));
+bandas.addBand(new Band('System of Dowm'));
+console.log(bandas);
 
 io.on('connection', client => {
    
     console.log('Cliente conectado');
+
+    client.emit('bandasactivas',bandas.getBands());
    client.on('disconnect', () => { 
      console.log('Cliente desconectado');
     });
@@ -13,6 +25,29 @@ io.on('connection', client => {
  
  
       io.emit('mensaje emitido2',{admin: 'numero mensaje'});
+    });
+
+    client.on('emitir-mensaje',(payload)=>{
+      console.log(payload);
+      io.emit('new-mensaje',payload);
+     
+    });
+
+    client.on('votando-bandas', function(paylod){
+      bandas.voteBand(paylod.id);
+      io.emit('bandasactivas',bandas.getBands());
+    });
+
+    client.on('añandiendo-banda',function(payload){
+      const bandanew=new Band(payload.name);
+      bandas.addBand(bandanew);
+      io.emit('bandasactivas',bandas.getBands())
+    });
+
+    client.on('eliminate-banda', function(payload){
+      bandas.deleteBand(payload.id);
+      io.emit('bandasactivas',bandas.getBands());
+      
     });
  
  
